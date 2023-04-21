8d341d3633c70125b52eddae1253965326181485
clementboloch <clement.boloch@gmail.com>
2023-04-21 09:58:13 +0200

    creates empty views and their URLs

--- src/review/urls.py
+++ src/review/urls.py
@@ -1,4 +1,14 @@
 from django.urls import path
+from . import views
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/review/login/'), name='root'),
+    path('login/', views.login, name='login'),
+    path('signup/', views.signup, name='signup'),
+    path('home/', views.home, name='home'),
+    path('follow/', views.follow, name='follow'),
+    path('ticket/', views.ticket, name='ticket'),
+    path('review/', views.review, name='review'),
+    path('posts/', views.posts, name='my_posts'),
 ]
--- src/review/views.py
+++ src/review/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# 1. Login view
+def login(request):
+    return HttpResponse("Login")
+
+
+# 2. Signup view
+def signup(request):
+    return HttpResponse("Signup")
+
+
+# 3. Home view
+def home(request):
+    return HttpResponse("Home")
+
+
+# 4. Follow view
+def follow(request):
+    return HttpResponse("Follow")
+
+
+# 5. Ticket creation view
+def ticket(request):
+    return HttpResponse("Ticket creation")
+
+
+# 6. Review creation view
+def review(request):
+    return HttpResponse("Review creation")
+
+
+# 7. My posts view
+def posts(request):
+    return HttpResponse("My posts")
